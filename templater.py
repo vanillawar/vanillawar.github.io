7cec79bc874290634bb8d3439b174c8f34a0cb37
Drak <warlorddrak@users.noreply.github.com>
2019-08-19 13:42:17 +0200

    Recursive templater with header/footer skip option and relative paths

--- templater.py
+++ templater.py
@@ -2,6 +2,7 @@
 Author: Warlord Drak
 '''
 
+import glob
 import json
 import os
 import time
@@ -12,6 +13,10 @@ def template(snippet):
         title += " | Vanilla Warrior"
     else:
         title = "Vanilla Warrior"
+    rel = snippet.meta.get('rel', '')
+    show_header = snippet.meta.get('header', True)
+    show_footer = snippet.meta.get('footer', True)
+    footer_link = '#' if show_header else 'https://www.vanillawar.com'
     return f'''<!DOCTYPE html>
 <html>
 
@@ -20,8 +25,8 @@ def template(snippet):
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="Content-Security-Policy" content="default-src 'self' 'unsafe-inline' 'unsafe-eval' *;">
   <link rel="stylesheet" href="https://cdn.rawgit.com/Chalarangelo/mini.css/v3.0.1/dist/mini-default.min.css" />
-  <link rel="stylesheet" href="static/vanillawar.css" />
-  <link rel="shortcut icon" type="image/png" href="static/logo.png" />
+  <link rel="stylesheet" href="{rel}static/vanillawar.css" />
+  <link rel="shortcut icon" type="image/png" href="{rel}static/logo.png" />
   <title>{title}</title>
   <meta name="description" content="World of Warcraft: Classic (Vanilla) Warrior guides and resources!" />
   <meta name="robots" content="index, follow" />
@@ -36,45 +41,45 @@ def template(snippet):
   <meta property="twitter:site" content="@WarlordDrak" />
   <meta property="twitter:image" content="https://www.vanillawar.com/static/logo.png" />
 
-  <script src="static/js/timer.js"></script>
+  <script src="{rel}static/js/timer.js"></script>
 
   <script>var whTooltips = ''' + '{colorLinks: true, iconizeLinks: true, renameLinks: true, hide: {ilvl: true}}' + f''';</script>
   <script src="https://wow.zamimg.com/widgets/power.js"></script>
 </head>
 
 <body>
- <div class="container">
-  <a href="https://www.vanillawar.com/">
+ <div class="container">''' + \
+  (f'''\n  <a href="https://www.vanillawar.com">
     <div class="banner">
-      <img src="static/vanillawar_banner_logo_w_icon_96px.png" alt="Vanilla Warrior Text Logo">
+      <img src="{rel}static/vanillawar_banner_logo_w_icon_96px.png" alt="Vanilla Warrior Text Logo">
     </div>
   </a>
 
   <nav class="row">
-    <span class="col-sm"><a href="level.html">Level</a></span>
-    <span class="col-sm"><a href="talents.html">Talents</a></span>
-    <span class="col-sm"><a href="pve-gear.html">PvE/Gear</a></span>
-    <span class="col-sm"><a href="pvp.html">PvP</a></span>
-    <span class="col-sm"><a href="gold.html">Gold</a></span>
-    <span class="col-sm"><a href="ui-macros.html">UI/Macros</a></span>
-    <span class="col-sm"><a href="tools.html">Tools</a></span>
-    <span class="col-sm"><a href="glossary.html">Glossary</a></span>
-  </nav>
+    <span class="col-sm"><a href="{rel}level.html">Level</a></span>
+    <span class="col-sm"><a href="{rel}talents.html">Talents</a></span>
+    <span class="col-sm"><a href="{rel}pve-gear.html">PvE/Gear</a></span>
+    <span class="col-sm"><a href="{rel}pvp.html">PvP</a></span>
+    <span class="col-sm"><a href="{rel}gold.html">Gold</a></span>
+    <span class="col-sm"><a href="{rel}ui-macros.html">UI/Macros</a></span>
+    <span class="col-sm"><a href="{rel}tools.html">Tools</a></span>
+    <span class="col-sm"><a href="{rel}glossary.html">Glossary</a></span>
+  </nav>''' if show_header else '') + f'''
 
 {snippet.content}
 
-  <footer class="row">
+  ''' + (f'''<footer class="row">
     <div class="col-sm">
       <p>
         <small>
-          © 2018-2019 by <a href="about.html">Drak</a> | <a href="about.html#legal">About</a>
+          © 2018-2019 by <a href="{rel}about.html">Drak</a> | <a href="{rel}about.html#legal">About</a>
         </small>
       </p>
       <p>
-      <a href="#"><img src="static/logo.png" src="VanillaWar.com icon" /></a>
+      <a href="{footer_link}"><img src="{rel}static/logo.png" src="VanillaWar.com icon" /></a>
       </p>
     </div>
-  </footer>
+  </footer>''' if show_footer else '') + f'''
  </div>
 </body>
 
@@ -87,16 +92,16 @@ class Snippet(object):
         self.content = content
 
 def main():
-    snippet_files = [filename for filename in os.listdir('.') if is_snippet(filename)]
+    snippet_files = [path for path in glob.glob('**/*.html', recursive=True) if is_snippet(path)]
     for snippet_file in snippet_files:
         snippet = read_file(snippet_file)
         html_file = target_filename(snippet_file)
-        print(time.ctime(os.path.getmtime(snippet_file)) + " " + html_file)
+        print(time.ctime(os.path.getmtime(snippet_file)) + " " + snippet_file + " " + html_file)
         rendered = template(snippet)
         write_file(html_file, rendered)
 
-def is_snippet(filename):
-    return filename.startswith('snippet.')
+def is_snippet(path):
+    return path.split('/')[-1].startswith('snippet.')
 
 def target_filename(snippet_filename):
     return snippet_filename.replace('snippet.', '')
