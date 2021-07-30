bd6878d314efbb5a13dc1b2f8fe5a3a2f45d5ed5
Drak <warlorddrak@users.noreply.github.com>
2021-07-30 15:02:50 +0200

    Add checklist

--- templater.py
+++ templater.py
@@ -64,6 +64,7 @@ def template(snippet):
     -->
     <span class="col-sm"><a href="{rel}tools/honor/">Honor Calculator</a></span>
     <span class="col-sm"><a href="{rel}tools/honor-pool/">Honor Pool Estimation</a></span>
+    <span class="col-sm"><a href="{rel}checklist/">Checklist</a></span>
     <span class="col-sm"><a href="{rel}ui-macros.html">UI/Macros</a></span>
     <span class="col-sm"><a href="{rel}glossary.html">Glossary</a></span>
   <!--
